5470722b77295f7508d2199d6057c68885a8d763
KCkingcollin <kckingcollin99@gmail.com>
2025-07-17 03:56:57 -0400

    cleaned up code, and added a full size dict gob to prevent as much downloading

--- wordle-helper.go
+++ wordle-helper.go
@@ -7,6 +7,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"math"
 	"net/http"
 	"os"
 	"sort"
@@ -17,21 +18,42 @@ import (
 	"github.com/gdamore/tcell/v2"
 )
 
-type word struct{
+const (
+	// number characters in the lowercase alphabet
+	alphaSize = int(26)
+	// default word size
+	defWsize = int(5)
+)
+
+var (
+	// 2D searchMap data structure that has sorted words (represented as uint16 index) in the wordDict
+	// into different maps each representing a set of words with a rune in the position
+	//
+	// the first dimension represents the position in the word.
+	//
+	// the second dimension represents the list of words with the alpha character (represented as a int from 0-25) in that x position
+	searchMap 				[][alphaSize]map[uint16]struct{}
+	wordDict 				[]word
+	setEmpty 				= struct{}{}
+	screen 					tcell.Screen
+	defaultStyle 			= tcell.StyleDefault.Background(tcell.ColorReset).Foreground(tcell.ColorWhite)
+	posIs, isIn, notIn 		string
+	mainTextBuf 			[][]rune
+	ev 						tcell.Event
+)
+
+type word struct {
 	W 		string
 	Rank 	uint16
 }
 
-var (
-	screen 				tcell.Screen
-	defaultStyle 		= tcell.StyleDefault.Background(tcell.ColorReset).Foreground(tcell.ColorWhite)
-	posIs, isIn, notIn 	string
-	search 				[][26]map[uint16]uint8
-	dict 				[]word
-	mainTextBuf 		[][]rune
-	ev 					tcell.Event
-)
+func isError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
+// wip and not ready for use
 func input() {
 	ev = screen.PollEvent()
 	switch ev := ev.(type) {
@@ -59,8 +81,7 @@ func input() {
 	}
 }
 
-
-// drawText function (modified to take screen explicitly)
+// wip and not ready for use
 func drawText(text string) {
 	sizeX, sizeY := screen.Size()
 	for _, r := range text {
@@ -88,192 +109,207 @@ func drawText(text string) {
 	screen.Show()
 }
 
-func containsAny(s, targetBytes string) bool {
-	if len(targetBytes) == 0 {return false}
-	byteSet := make(map[rune]bool)
-	for _, r := range targetBytes {
-		if int(r) <= 123 && int(r) >= 97 {
-			byteSet[r] = true
+func containsAny(s, targetString string) bool {
+	if len(targetString) == 0 {return false}
+	runeSet := make(map[rune]bool)
+	for _, r := range targetString {
+		if r >= 'a' && r <= 'z' {
+			runeSet[r] = true
 		}
 	}
 	for _, r := range s {
-		if byteSet[r] {return true}
+		if runeSet[r] {return true}
 	}
 	return false
 }
 
-func containsAll(s, requiredBytes string) bool {
-	if len(requiredBytes) == 0 {return true}
-	for i, r := range requiredBytes {
-		if int(r) <= 123 && int(r) >= 97 {
+func containsAll(s, requiredRunes string) bool {
+	// need to alter how this function works so that you can have 2 characters in the same position
+	if len(requiredRunes) == 0 {return true}
+	for i, r := range requiredRunes {
+		if r >= 'a' && r <= 'z' {
 			if s[i] == byte(r) {return false}
 		}
 	}
-	foundBytes := make(map[rune]bool)
-	for _, r := range requiredBytes {
-		if int(r) <= 123 && int(r) >= 97 {
-			foundBytes[r] = false
+	foundRunes := make(map[rune]bool)
+	for _, r := range requiredRunes {
+		if r >= 'a' && r <= 'z' {
+			foundRunes[r] = false
 		}
 	}
 
-	for _, byteInS := range s {
-		if _, ok := foundBytes[byteInS]; ok {
-			foundBytes[byteInS] = true
+	for _, r := range s {
+		if _, ok := foundRunes[r]; ok {
+			foundRunes[r] = true
 		}
 	}
 
-	for _, found := range foundBytes {
+	for _, found := range foundRunes {
 		if !found {return false}
 	}
 	return true
 }
 
 func runSearch(notInRaw ...bool) {
-		if len(posIs) <= 0 {posIs+="_"}
-		size := len(search)
-		if len(posIs) > size {posIs = posIs[:size]}
-		if len(isIn) > size {isIn = isIn[:size]}
-		list := make([]map[uint16]uint8, len(posIs))
-		add2List := func(i int, j uint16) {
-			ok := true
-			if i > 0 {_, ok = list[i-1][j]}
-			if ok && containsAll(dict[int(j)].W[:size], isIn) && !containsAny(dict[int(j)].W[:size], notIn) {
-				list[i][j]++
-			}
+	if len(posIs) <= 0 {posIs+="_"}
+	size := len(searchMap)
+	if len(posIs) > size {posIs = posIs[:size]}
+	if len(isIn) > size {isIn = isIn[:size]}
+	list := make([]map[uint16]struct{}, len(posIs))
+	add2List := func(i int, j uint16) {
+		ok := true
+		if i > 0 {_, ok = list[i-1][j]}
+		if ok && containsAll(wordDict[int(j)].W[:size], isIn) && !containsAny(wordDict[int(j)].W[:size], notIn) {
+			list[i][j] = setEmpty
 		}
-		for i, r := range []rune(posIs) {
-			list[i] = make(map[uint16]uint8)
-			if int(r) <= 123 && int(r) >= 97 {
-				for j := range search[i][int(r-97)] {
-					add2List(i, j)
-				}
-			} else {
-				for k := range 26 {
-					if !containsAny(string(rune(k+97)), notIn) {
-						for j := range search[i][k] {
-							add2List(i, j)
-						}
+	}
+	for i, r := range []rune(posIs) {
+		list[i] = make(map[uint16]struct{})
+		if r >= 'a' && r <= 'z' {
+			for j := range searchMap[i][int(r-'a')] {
+				add2List(i, j)
+			}
+		} else {
+			for k := range alphaSize {
+				if !containsAny(string(rune(k+'a')), notIn) {
+					for j := range searchMap[i][k] {
+						add2List(i, j)
 					}
 				}
 			}
 		}
-		output := make([]word, len(list[len(list)-1]))
-		var i int
-		for s := range list[len(list)-1] {
-			output[i] = dict[s]
-			i++
-		}
-		sort.Slice(output, func(i, j int) bool {
-			return output[i].Rank > output[j].Rank
-		})
-		var textBuf string
-		for _, r := range output {
-			textBuf += string(r.W[:size])+"\n"
-			// textBuf += string(r.W[:size])+" "
-			// textBuf += fmt.Sprintln(r.Rank)
-		}
-		if len(notInRaw) > 0 && notInRaw[0] {
-			fmt.Print(textBuf)
-		} else {
-			drawText(textBuf)
-		}
-}
-
-func isError(err error) {
-	if err != nil {
-		panic(err)
+	}
+	output := make([]word, len(list[len(list)-1]))
+	var i int
+	for s := range list[len(list)-1] {
+		output[i] = wordDict[s]
+		i++
+	}
+	sort.Slice(output, func(i, j int) bool {
+		return output[i].Rank > output[j].Rank
+	})
+	var textBuf string
+	for _, r := range output {
+		textBuf += string(r.W[:size])+"\n"
+	}
+	if len(notInRaw) > 0 && notInRaw[0] {
+		fmt.Print(textBuf)
+	} else {
+		drawText(textBuf)
 	}
 }
 
-func createDict(wordSize int) {
-		dict = make([]word, 0)
-		resp, err := http.Get("https://raw.githubusercontent.com/dwyl/english-words/refs/heads/master/words_alpha.txt")
-		isError(err)
-		defer resp.Body.Close()
-		tempBuf, err := io.ReadAll(resp.Body)
-		isError(err)
-		buf := bytes.NewReader(tempBuf)
-		reader := bufio.NewReader(buf)
-		resp, err = http.Get("https://raw.githubusercontent.com/david47k/top-english-wordlists/refs/heads/master/top_english_words_mixed_100000.txt")
-		isError(err)
-		rankTempBuf, err := io.ReadAll(resp.Body)
-		isError(err)
-		rankBuf := bytes.NewReader(rankTempBuf)
-		rankReader := bufio.NewReader(rankBuf)
-		wordRank := make(map[string]uint16)
-		var i uint16
-		for {
-			s, err := rankReader.ReadString('\n')
-			if err != nil {
-				break
-			}
-			s = strings.TrimSpace(s)
-			if len(s) == wordSize {
-				i++
-				wordRank[s] = i
-			}
+func createDict() []word {
+	var dict []word
+	resp, err := http.Get("https://raw.githubusercontent.com/dwyl/english-words/refs/heads/master/words_alpha.txt")
+	isError(err)
+	defer resp.Body.Close()
+	tempBuf, err := io.ReadAll(resp.Body)
+	isError(err)
+	buf := bytes.NewReader(tempBuf)
+	reader := bufio.NewReader(buf)
+	resp, err = http.Get("https://raw.githubusercontent.com/david47k/top-english-wordlists/refs/heads/master/top_english_words_mixed_100000.txt")
+	isError(err)
+	rankTempBuf, err := io.ReadAll(resp.Body)
+	isError(err)
+	rankBuf := bytes.NewReader(rankTempBuf)
+	rankReader := bufio.NewReader(rankBuf)
+	wordRank := make(map[string]uint16)
+	var i uint16
+	for {
+		s, err := rankReader.ReadString('\n')
+		if err != nil {
+			break
 		}
-		for {
-			b, err := reader.ReadString('\n')
-			if err != nil {
-				break
-			}
-			b = strings.TrimSpace(b)
-			if len(b) == wordSize {
-				var word word
-				word.W = b
-				_, ok := wordRank[word.W]
-				if !ok {
-					word.Rank = 65535
-				} else {
-					word.Rank = wordRank[word.W]
-				}
-				dict = append(dict, word)
-			}
+		s = strings.TrimSpace(s)
+		i++
+		wordRank[s] = i
+	}
+	for {
+		b, err := reader.ReadString('\n')
+		if err != nil {
+			break
 		}
-		file, err := os.Create("dict.gob")
-		defer file.Close()
-		isError(err)
-		encoder := gob.NewEncoder(file)
-		encoder.Encode(dict)
+		b = strings.TrimSpace(b)
+		var word word
+		word.W = b
+		_, ok := wordRank[word.W]
+		if !ok {
+			word.Rank = math.MaxUint16
+		} else {
+			word.Rank = wordRank[word.W]
+		}
+		dict = append(dict, word)
+	}
+	file, err := os.Create("dict.gob")
+	defer file.Close()
+	isError(err)
+	encoder := gob.NewEncoder(file)
+	encoder.Encode(dict)
+	return dict
+}
+
+func createWordDict(wordSize int, dict []word) {
+	wordDict = make([]word, 0)
+	for _, word := range dict {
+		if len(word.W) == wordSize {
+			wordDict = append(wordDict, word)
+		}
+	}
+	file, err := os.Create("word-dict.gob")
+	defer file.Close()
+	isError(err)
+	encoder := gob.NewEncoder(file)
+	encoder.Encode(wordDict)
 }
 
 func createSearch(wordSize int) {
-		search = make([][26]map[uint16]uint8, wordSize)
-		for i := range wordSize {
-			for charNum := range 26 {
-				search[i][charNum] = make(map[uint16]uint8)
-				for j, r := range dict {
-					if strings.Contains(string(r.W[i]), string(rune(charNum+97))) {
-						search[i][charNum][uint16(j)]++
-					}
+	searchMap = make([][alphaSize]map[uint16]struct{}, wordSize)
+	for i := range wordSize {
+		for charNum := range alphaSize {
+			searchMap[i][charNum] = make(map[uint16]struct{})
+			for j, r := range wordDict {
+				if strings.Contains(string(r.W[i]), string(rune(charNum+'a'))) {
+					searchMap[i][charNum][uint16(j)] = setEmpty
 				}
 			}
 		}
-		file, err := os.Create("search.gob")
-		defer file.Close()
-		isError(err)
-		encoder := gob.NewEncoder(file)
-		encoder.Encode(search)
+	}
+	file, err := os.Create("search-map.gob")
+	defer file.Close()
+	isError(err)
+	encoder := gob.NewEncoder(file)
+	encoder.Encode(searchMap)
 }
 
 func main() {
+	var dict []word
 	file, err := os.Open("dict.gob")
 	defer file.Close()
 	if errors.Is(err, os.ErrNotExist) {
-		createDict(5)
+		dict = createDict()
 	} else {
 		decode := gob.NewDecoder(file)
 		err := decode.Decode(&dict)
 		isError(err)
 	}
 
-	file, err = os.Open("search.gob")
+	file, err = os.Open("word-dict.gob")
+	defer file.Close()
 	if errors.Is(err, os.ErrNotExist) {
-		createSearch(5)
+		createWordDict(defWsize, dict)
 	} else {
 		decode := gob.NewDecoder(file)
-		err := decode.Decode(&search)
+		err := decode.Decode(&wordDict)
+		isError(err)
+	}
+
+	file, err = os.Open("search-map.gob")
+	if errors.Is(err, os.ErrNotExist) {
+		createSearch(defWsize)
+	} else {
+		decode := gob.NewDecoder(file)
+		err := decode.Decode(&searchMap)
 		isError(err)
 	}
 
@@ -298,8 +334,8 @@ func main() {
 				return
 			case err == nil:
 				wordSize = size
-				if len(search) != wordSize {
-					createDict(wordSize)
+				if len(searchMap) != wordSize {
+					createWordDict(wordSize, dict)
 					createSearch(wordSize)
 				}
 			default:
@@ -310,6 +346,8 @@ func main() {
 				posIs = strings.TrimSpace(a)
 				posIs = strings.ToLower(posIs)
 			case 2:
+				// need to alter how this argument works so that you can have 2 characters in the same position
+				// most likely with a 2d slice of runes so that each position can have more than 1 rune associated to it
 				isIn = strings.TrimSpace(a)
 				isIn = strings.ToLower(isIn)
 			case 3:
@@ -320,9 +358,9 @@ func main() {
 				break outer
 			}
 		}
-		if len(search) != 5 && wordSize == 0 {
-			createDict(5)
-			createSearch(5)
+		if len(searchMap) != defWsize && wordSize == 0 {
+			createWordDict(defWsize, dict)
+			createSearch(defWsize)
 		}
 		runSearch(true)
 		return
@@ -331,12 +369,12 @@ func main() {
 	screen, err = tcell.NewScreen()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to create screen: %v\n", err)
-		os.Exit(1)
+		return
 	}
 
 	if err = screen.Init(); err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to initialize screen: %v\n", err)
-		os.Exit(1)
+		return
 	}
 	defer screen.Fini()
 
